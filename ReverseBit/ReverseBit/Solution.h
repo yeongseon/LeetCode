f898dd0f73b9f6311dd9fe230f4a232d67daae5a
yeongseon choe <yeongseon.choe@gmail.com>
2016-04-26 11:24:29 +0900

    Solve the problem

--- ReverseBit/ReverseBit/Solution.h
+++ ReverseBit/ReverseBit/Solution.h
@@ -1,13 +1,38 @@
 #pragma once
+#include <iostream>
 #include <stdint.h>
 
+using namespace std;
 class Solution
 {
 public:
 	Solution();
 	~Solution();
 	uint32_t reverseBits(uint32_t n) {
+		int inputarray[32] = { 0, };
+		int index = 0;
 
+		if (n == 0)
+			return 0;
+
+		while (n!= 1) {			
+			inputarray[index] = n % 2;
+			index++;
+			n = n / 2;
+		}
+		inputarray[index] = n % 2;
+		index++;
+
+		uint32_t bit = 1;
+		uint32_t output = 0;
+
+		for (int i = 0; i < 32; i++) {
+			if (inputarray[31 - i])
+				output += bit;
+			bit = bit << 1;
+		}
+
+		return output;
 	}
 };
 
--- ReverseBit/ReverseBit/main.cpp
+++ ReverseBit/ReverseBit/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include "Solution.h"
 
 using namespace std;
 int main() {
-	cout << "Hello World" << endl;
+	Solution solution;
+	cout << solution.reverseBits(43261596) << endl;
+	cout << solution.reverseBits(00000000000000000000000000000000) << endl;
+	
 	return 0;
 }
